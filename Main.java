760e843da74b76bb59a990be7f97b4ed7e652260
rzhu-0812 <86477098+rzhu-0812@users.noreply.github.com>
2024-09-06 12:23:31 -0400

    1.1 Why Programming? Why Java?

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello!");
-        System.out.print("My name is Ryan!");
+        System.out.println("Prints with new line");
+        System.out.print("Prints without new line");
     }
 }
